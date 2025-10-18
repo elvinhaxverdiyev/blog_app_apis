e8738fa02b340c86440af730a8ff1415f4b9d75c
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-10-18 12:55:07 +0400

    fixed some bugs

--- blog/apis/user_apis/users_apis.py
+++ blog/apis/user_apis/users_apis.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.shortcuts import get_object_or_404
+from rest_framework.permissions import AllowAny
 
 from users.models.user_models import CustomUser
 from users.serializers.user_serializers import CustomUserSerializer
@@ -17,6 +17,7 @@ class UserListAPIView(APIView):
     API View to list all registered users.
     Accepts only GET requests.
     """
+    permission_classes = [AllowAny]
     http_method_names = ["get"]
     
     def get(self, request):
