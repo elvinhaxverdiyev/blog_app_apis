b1e5d8e04377d8be7e15d47813d94486cc356c11
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-10-18 13:18:58 +0400

    fixed some bugs

--- blog/utils/validations.py
+++ blog/utils/validations.py
@@ -1,3 +1,6 @@
-def check_password_length(password):
-    
+def check_password_length(password: str)-> bool:
+    """
+    Validates that the password length is between 4 and 10 characters.
+    Returns True if valid, False otherwise.
+    """
     return 4 <= len(password) <= 10
