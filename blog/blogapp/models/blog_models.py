c2fff1bbc5feea65af08641cc6215f3a3ce703ed
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-10-30 21:13:18 +0400

    added dogstring for blog model

--- blog/blogapp/models/blog_models.py
+++ blog/blogapp/models/blog_models.py
@@ -1,7 +1,15 @@
 from django.db import models
 
-
 class Blog(models.Model):
+    """
+    Represents a blog post.
+
+    Attributes:
+        title (CharField): The title of the blog post.
+        text (TextField): The main content of the blog post.
+        created_at (DateTimeField): When the post was created.
+        updated_at (DateTimeField): When the post was last updated.
+    """
     title = models.CharField(max_length=255, verbose_name="Bashliq")
     text = models.TextField(max_length=10000, verbose_name="Metn")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Yaradilma Tarixi")
