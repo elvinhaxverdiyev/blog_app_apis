b757cb7b57d3f0cc51643762d2a6a0dac59a8fea
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-10-30 21:14:21 +0400

    added dogstring for blogimage  model

--- blog/blogapp/models/blog_images_models.py
+++ blog/blogapp/models/blog_images_models.py
@@ -3,6 +3,14 @@ from django.db import models
 from .blog_models import Blog
 
 class BlogImagesModel(models.Model):
+    """
+    Represents an image associated with a blog post.
+
+    Attributes:
+        blog (ForeignKey): The blog post this image belongs to.
+        image (ImageField): The image file, optional.
+        created_at (DateTimeField): Timestamp when the image was created.
+    """
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(verbose_name="Blog Shekili", blank=True, null=True, upload_to="blog_images/")
     created_at = models.DateTimeField(auto_now_add=True)
