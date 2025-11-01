6f449d675f662955b4e7b5328b801cb042019c2e
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-11-01 18:49:46 +0400

    added dog string for user image model

--- blog/users/models/user_image_model.py
+++ blog/users/models/user_image_model.py
@@ -4,6 +4,7 @@ from .user_models import CustomUser
 
 
 class UsersImageModel(models.Model):
+    """Stores user profile images."""
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(verbose_name="Profil Şəkli", blank=True, null=True, upload_to="profil_images/")
     created_at = models.DateTimeField(auto_now_add=True)
