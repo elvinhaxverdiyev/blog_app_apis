36f8bda28d91190c0b3da6a58cf8e34274a39f2a
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-11-01 18:52:35 +0400

    added dog string for user serialzier

--- blog/users/serializers/user_serializers.py
+++ blog/users/serializers/user_serializers.py
@@ -4,6 +4,7 @@ from users.models.user_models import CustomUser
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
+    """Serializer for the CustomUser model."""
     class Meta:
         model = CustomUser
         fields = "__all__"
