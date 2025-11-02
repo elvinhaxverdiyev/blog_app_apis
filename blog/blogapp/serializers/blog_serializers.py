2660a462558372219a46e3ac4e92d1cfd5888042
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-11-02 20:02:49 +0400

    added dog string for blog serializer

--- blog/blogapp/serializers/blog_serializers.py
+++ blog/blogapp/serializers/blog_serializers.py
@@ -4,6 +4,11 @@ from blogapp.models.blog_models import Blog
 
 
 class BlogSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Blog model.
+    Handles conversion between Blog instances and JSON representations,
+    including title, text, image, created_at, and updated_at fields.
+    """
     class Meta:
         model = Blog
         fields = [
