403415eb1e07b662976a5452ea8e2ec925f563c0
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-11-02 20:01:42 +0400

    added dog string for blog image serializer

--- blog/blogapp/serializers/blog_images_serializers.py
+++ blog/blogapp/serializers/blog_images_serializers.py
@@ -4,6 +4,11 @@ from blogapp.models.blog_images_models import BlogImagesModel
 
 
 class BlogImageSerializer(serializers.ModelSerializer):
+    """
+    Serializer for BlogImagesModel.
+    Provides serialization and deserialization of blog image data,
+    including all model fields.
+    """
     class Meta:
         model = BlogImagesModel
         fields = "__all__"
