be11506d3dd84eaeb14b47f3a87feef6e78c1259
Elvin Haxverdiyev <elvinhaxverdiyev777@github.com>
2025-11-04 23:21:22 +0400

    added admin panel for user model

--- blog/users/admin.py
+++ blog/users/admin.py
@@ -8,3 +8,31 @@ class UsersImageAdmin(admin.ModelAdmin):
     list_filter = ("created_at",)
     search_fields = ("user__username", "user__name")
     ordering = ("-created_at",)
+
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models.user_models import CustomUser
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "username", "name", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_joined")
+    search_fields = ("email", "username", "name")
+    ordering = ("-date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("username", "name", "bio")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "username", "name", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
